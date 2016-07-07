bb1549960fca151827ef85903d702b365d938d9c
Phelps17 <Phelps3@wisc.edu>
2016-07-06 23:51:50 -0500

    updated weather controller

--- weather_controller.py
+++ weather_controller.py
@@ -26,24 +26,88 @@ class WeatherController:
 		parsed_json = self._parse_weather_json(json_string)
 		self.weather_object = WeatherObject(parsed_json, location)
 
-	def get_current_conditions(self, location) :
+	def _check_if_location_needs_update(self, location):
 		if (self.weather_object.is_populated is not True) :
 			self._update_weather_object(location)
 		elif (self.weather_object.location_string != location) :
 			if (len(location) > 0) :
 				self._update_weather_object(location)
 
+	def get_current_conditions(self, location) :
+		self._check_if_location_needs_update(location)
 		return self.weather_object.get_current_conditions()
 
 	def get_forecast_conditions(self, location) :
-		if (self.weather_object.is_populated is not True) :
-			self._update_weather_object(location)
-		elif (self.weather_object.location_string != location) :
-			if (len(location) > 0) :
-				self._update_weather_object(location)
-
+		self._check_if_location_needs_update(location)
 		return self.weather_object.get_forecast_conditions()
 
+	def get_current_title(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_current_title()
+
+	def get_current_temp(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_current_temp()
+
+	def get_current_condition_text(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_current_condition_text()
+
+	def get_temp_units(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_temp_units()
+
+	def get_distance_units(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_pressure_units()
+
+	def get_pressure_units(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.)
+
+	def get_speed_units(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_speed_units()
+
+	def get_weather_on_day(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_weather_on_day()
+
+	def get_sunrise_time(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_sunrise_time()
+
+	def get_sunset_time(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_sunset_time()
+
+	def get_wind_chill(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_wind_chill()
+
+	def get_wind_direction(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_wind_direction()
+
+	def get_wind_speed(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_wind_speed()
+
+	def get_atmosphere_humidity(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_atmosphere_humidity()
+
+	def get_atmosphere_pressure(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_atmosphere_pressure()
+
+	def get_atmosphere_rising(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_atmosphere_rising()
+
+	def get_atmosphere_visibility(self, location) :
+		self._check_if_location_needs_update(location)
+		return self.weather_object.get_atmosphere_visibility()
 
 
 
