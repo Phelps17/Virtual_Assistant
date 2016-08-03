648bf86f0f2a42cd5683a87b560b2ed08d1a8b71
Phelps17 <Phelps3@wisc.edu>
2016-08-02 23:32:39 -0500

    twitter skeleton

---
+++ twitter_controller.py
@@ -0,0 +1,28 @@
+import tweepy
+
+CONSUMER_KEY = "your key here"
+CONSUMER_SECRET = "your key here"
+ACCESS_TOKEN = "your key here"
+ACCESS_TOKEN_SECRET = "your key here"
+
+class TwitterController() :
+	def __inti__(self) :
+		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+		auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+		self.api = tweepy.API(auth)
+
+	def get_timeline_tweets(self) :
+		pass
+
+	def get_my_tweets(self) :
+		pass
+
+	def get_tweets_of_user(self, other_username) :
+		account_name = self._get_user_twitter_name(other_username)
+		pass
+
+	def _get_user_twitter_name(self, other_user_name) :
+		pass
+
+	def tweet(self, tweet) :
+		pass
