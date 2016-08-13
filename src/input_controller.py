1eb631a33954e3fbe37d197bd118ad04b2a118be
Phelps17 <Phelps3@wisc.edu>
2016-08-13 01:24:17 -0500

    some pre-nltk work

--- src/input_controller.py
+++ src/input_controller.py
@@ -6,17 +6,24 @@ class InputController :
 	def get_input(self) :
 		# get the input from either cmd or mic
 		if (self.input_mode == 0) :
-			#print "command arguments"
-			return
+			return raw_input()
 		elif (self.input_mode == 1) :
-			#print "voice arguments (not implemented)"
+			print "voice arguments (not implemented)"
 			return
 		else :
-			#print "default (command arguments)"
-			return
+			return raw_input()
 
 	def process_input(self, input_string) :
-		# figure out what command to run
-		# return the command name in a string array
-		# string at 0 and args after that
+		print input_string
+		
+		# nltk split up sentence for keywords and arguments
+		# try to find super keys
+		# cross refernce found keywords with cached commands
+		# cross reference with non-cached keywords
+		# determine the command to be ran
+		# determine the arguments and types of arguments needed
+		# determine those arguments
+		# order command and args into a strng array
+		# return that array
+
 		pass
--- src/virtual_assistant.py
+++ src/virtual_assistant.py
@@ -8,14 +8,13 @@ from tts.linux_tts import *
 from tts.no_tts import *
 from input_controller import *
 
-# In Progress API Functions:
-#	- facebook
-#	- twitter
-# Future API Functions:
-#	- calendar
-#	- email
-#	- recent news
-#	- alarms
+KILL_STRING = "die" #TODO - add to config
+
+def cleanup_and_exit() :
+	exit()
+
+def run_command() :
+	pass
 
 # ================ SET UP ================
 reply = ReplyController()
@@ -23,18 +22,23 @@ weather = WeatherController()
 user = load_user()
 clock = DateTimeController()
 voice = NoTextToSpeech()
-io = InputController(0)
+io = InputController(0) #TODO - add to config
 
 if user.get_system_type() == 'MAC' :
 	voice = MacTextToSpeech()
 elif user.get_system_type() == 'LINUX' :
 	voice = LinuxTextToSpeech()
 
-user.set_formality_level(2)
+user.set_formality_level(2) #TODO - add to config
 # ========================================
 
-io.get_input()
-
-#while (1) :
+while (1) :
 	# get input
-	# run command
+	input_string = io.get_input()
+	if (input_string == KILL_STRING) :
+		cleanup_and_exit()
+	else :
+		io.process_input(input_string)
+		run_command()
+
+exit()
